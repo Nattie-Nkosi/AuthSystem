59e193a1c4f337cfbd4cbe4c5e98f49edf891a0e
Nutty4616 <nkosin361@gmail.com>
2023-02-03 12:20:35 +0200

    Endpoint fixed

--- src/app.js
+++ src/app.js
@@ -1,23 +1,27 @@
+require("./models/User");
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
-const { registerRouter } = require("./routes/register");
-const { loginRouter } = require("./routes/login");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+
 const app = express();
 
-app.use(express.json());
-app.use(cors());
 app.use(bodyParser.json());
+app.use(authRoute);
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+try {
+  mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+} catch (error) {
+  console.error(`Failed to connect to MongoDB: ${error}`);
+}
 
 const connection = mongoose.connection;
 connection.on("error", console.error.bind(console, "connection error"));
@@ -25,14 +29,11 @@ connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
-app.use(registerRouter);
-app.use(loginRouter);
-
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Welcome to the authentication system");
 });
 
-app.use("*", (req, res) => {
+app.all("*", (req, res) => {
   res.status(404).send({ error: "Route not found" });
 });
 
---
+++ src/routes/auth.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const { check } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const validateRequest = require("../middlewares/validateRequest");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// @route   POST register
+// @desc    Register a user
+// @access  Public
+router.post(
+  "register",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  validateRequest,
+  async (req, res) => {
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+// @route   POST login
+// @desc    Login user / Returning JWT
+// @access  Public
+router.post(
+  "login",
+  [
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  validateRequest,
+  async function (req, res) {
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Credentials" });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
+
+module.exports = router;
